fb5df70381549cdc4412d055c242f6808cbce4da
roger <23081533@student.uwa.edu.au>
2022-05-18 00:58:25 +0800

    project2 framwork

--- app/routes.py
+++ app/routes.py
@@ -2,46 +2,42 @@ from app import app
 from flask import render_template, flash, redirect,url_for
 from app.forms import LoginForm
 
+
+
 @app.route('/')
 @app.route('/index')
-def index1():
-    user = {'username': 'm'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - rogerblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
-
-@app.route('/template')
 def index():
     user = {'username': 'roger'}
     return render_template('index.html', title='Home', user=user)
 
 @app.route('/game')
-def gme():
-	return "this is my game"
+def game():
+	user = {'username': 'Roger'}
+	return render_template('game.html', title='Play', user=user)
+	
+@app.route('/rule')
+def rule():
+	user = {'username': 'Roger'}
+	return render_template('rule.html', title='rule', user=user)
+	
 @app.route('/post')
 def index2():
     user = {'username': 'Roger'}
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'body': 'congradulations!!'
         },
         {
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'body': 'good job!'
         },
         {
 		'author': {'username': 'rogerson'},
-		'body' : 'this is try'
+		'body' : 'could you follow me and play with me'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('post.html', title='Message', user=user, posts=posts)
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
